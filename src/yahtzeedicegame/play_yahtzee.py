d55358311e96f77856b55f72f94a6afe52a841cd
bparento <sp3643@drexel.edu>
2023-11-07 23:28:04 -0500

    Added skeleton code to play_yahtzee

---
+++ src/yahtzeedicegame/play_yahtzee.py
@@ -0,0 +1,32 @@
+from player import Player
+from display_dice import display_dice
+
+class PlayYahtzee:
+    def __init__(self, player):
+        self.player = player
+    
+    def roll(self):
+        pass
+    
+    def choose_category(self):
+        pass
+
+    def start_new_turn(self):
+        pass
+
+    def turn(self):
+        pass
+
+    def play_game(self):
+        pass
+
+    def generate_block_art(self):
+        pass
+
+    def game_over(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = PlayYahtzee()
+    game.play_game()
