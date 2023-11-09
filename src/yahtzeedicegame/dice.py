3fec48bd073666c46fbdcada9287d19cfa7bf4da
bparento <sp3643@drexel.edu>
2023-11-09 13:19:25 -0500

    Updated dice.py

--- src/yahtzeedicegame/dice.py
+++ src/yahtzeedicegame/dice.py
@@ -1,25 +1,26 @@
 import random
 from dataclasses import dataclass, field
-from typing import List
 
+@dataclass
 class Dice:
-    def __init__(self, sides=6, current_value=None):
-        self.sides = sides
-        self.current_value = current_value
+    sides: int = 6
+    current_value: int = field(default=None, init=False)
 
     def roll(self):
-        self.current_value = random.randint(1,self.sides)
-        return self.current_value
+        self.current_value = random.randint(1, self.sides)
 
 class YahzeeDice(Dice):
     def __init__(self, num_dice=5):
-        Dice.__init__(self, sides=6, current_value=None)
+        super().__init__()  # Call the __init__ method of the parent class
         self.num_dice = num_dice
         self.dice_set = [Dice() for _ in range(self.num_dice)]
-    
-    def roll_all(self):
-        return [dice.roll() for dice in self.dice_set] # Rolls all the dice in dice_set and returns a list of the rolled values.
-    
-    def get_values(self):
-        return [dice.current_value for dice in self.dice_set] #Returns a list containing the current values of all dice in dice_set.
 
+    def roll_all(self):
+        [dice.roll() for dice in self.dice_set]
+
+    def get_values(self):
+        return [dice.current_value for dice in self.dice_set]
+
+test = YahzeeDice()
+test.roll_all()  # Roll the dice before getting values
+print(test.get_values())
